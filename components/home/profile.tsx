4190d3f9e3381659a6ccde5d9ed8dd086b39f47b
AlexTraveylan <monsieurdemares@gmail.com>
2024-09-12 11:23:53 +0200

    feat: update profile component to use local avatar image and improve image loading with width/height attributes

--- components/home/profile.tsx
+++ components/home/profile.tsx
@@ -3,12 +3,15 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 function Profile() {
-  const avatarUrl = "https://avatars.githubusercontent.com/u/108667536?v=4"
-
   return (
     <div className="flex items-center gap-4">
       <Avatar>
-        <AvatarImage src={avatarUrl} alt="Photo de profil" />
+        <AvatarImage
+          src={"/profil.webp"}
+          alt="Photo de profil"
+          width={40}
+          height={40}
+        />
         <AvatarFallback>VP</AvatarFallback>
       </Avatar>
       <div>Alex Traveylan</div>
